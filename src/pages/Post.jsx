ad7c4f747bdc83bcde6b82f4ebcdaaa36fa6199e
Alexis Castellanos <castellanos.alexis@hotmail.com>
2024-04-12 02:38:28 -0600

    use defer, await and LazyLoading

--- src/pages/Post.jsx
+++ src/pages/Post.jsx
@@ -1,16 +1,36 @@
-import { useLoaderData } from "react-router-dom";
-
+import { Await, defer, json, useLoaderData } from "react-router-dom";
 import PostItem from "../components/PostItem";
 
 function PostPage() {
-	const post = useLoaderData();
+	const { post } = useLoaderData();
 
-	return <PostItem post={post} />;
+	return (
+		<Await resolve={post}>
+			{(loaded_post) => <PostItem post={loaded_post} />}
+		</Await>
+	);
 }
 
+async function loadPost(post_id) {
+	const response = await fetch(
+		"https://jsonplaceholder.typicode.com/posts/" + post_id
+	);
+
+	if (response.ok) {
+		const response_data = await response.json();
+		return response_data;
+	} else {
+		throw json("Could not fetch post.", { status: 500 });
+	}
+}
+
+function loader({ params }) {
+	const post_id = params.id;
+
+	return defer({
+		post: loadPost(post_id),
+	});
+}
+
+export { loader };
 export default PostPage;
-
-export function loader({ params }) {
-	const postId = params.id;
-	return fetch("https://jsonplaceholder.typicode.com/posts/" + postId);
-}
